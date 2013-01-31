25e3706b6b028515fd29bc69f1cea689b3682ee2
Henry <hhrosvick@gmail.com>
2013-01-30 17:12:40 -0700

    Adding JUnit test.

---
+++ seng403/tests/MyRobotTestCases.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MyRobotTestCases {
+
+	@Test
+	public void test() {
+		fail("No idea how to test this system quite yet.");
+	}
+
+}
