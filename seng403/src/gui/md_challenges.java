c4f08906b00fe9847a27a17e758813d0d83f3bea
Marshall <Marshall@Marshall-PC>
2013-02-20 18:12:37 -0800

    Marshall

--- seng403/src/gui/md_challenges.java
+++ seng403/src/gui/md_challenges.java
@@ -2,6 +2,8 @@ package gui;
 
 import java.awt.EventQueue;
 import javax.swing.text.Element;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import javax.swing.JFrame;
 
 import casa.CASAProcess;
@@ -22,6 +24,7 @@ import javax.swing.JTabbedPane;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.JTree;
 import javax.swing.ScrollPaneConstants;
 
 import java.awt.Panel;
@@ -37,6 +40,7 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.TextArea;
 import java.awt.ScrollPane;
 import java.awt.Label;
@@ -396,7 +400,106 @@ public class md_challenges {
 /*****************************************************************************************************************************************
 * ChallengeTab initialization and components
 ******************************************************************************************************************************************/			
+
 		JPanel ChallengesTab = new JPanel();
 		TabPage.addTab("Challenges", null, ChallengesTab, null);
+		ChallengesTab.setLayout(new FormLayout(new ColumnSpec[] {
+				FormFactory.RELATED_GAP_COLSPEC,
+				FormFactory.DEFAULT_COLSPEC,
+				FormFactory.RELATED_GAP_COLSPEC,
+				ColumnSpec.decode("default:grow"),},
+			new RowSpec[] {
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				FormFactory.DEFAULT_ROWSPEC,
+				FormFactory.RELATED_GAP_ROWSPEC,
+				RowSpec.decode("default:grow"),}));
+	
+			
+			JScrollPane challengePane = new JScrollPane();
+			ChallengesTab.add(challengePane, "4, 2, 1, 31, fill, fill");
+			
+			JButton BeginChallenge = new JButton("Begin Challenge");
+			BeginChallenge.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+				
+				}
+			});
+			
+			
+			BeginChallenge.setFont(new Font("Helvetica Neue", Font.PLAIN, 13));
+			ChallengesTab.add(BeginChallenge, "2,2");
+			
+			JTree ChallengeTree = new JTree();
+			ChallengeTree.setModel(new DefaultTreeModel(
+				new DefaultMutableTreeNode("Challenges") {
+					{
+						DefaultMutableTreeNode node_1;
+						node_1 = new DefaultMutableTreeNode("Easy");
+							node_1.add(new DefaultMutableTreeNode("E: 1.0"));
+							node_1.add(new DefaultMutableTreeNode("E: 2.0"));
+							node_1.add(new DefaultMutableTreeNode("E: 3.0"));
+							node_1.add(new DefaultMutableTreeNode("E: 4.0"));
+						add(node_1);
+						node_1 = new DefaultMutableTreeNode("Medium");
+							node_1.add(new DefaultMutableTreeNode("M: 1.0"));
+							node_1.add(new DefaultMutableTreeNode("M: 2.0"));
+							node_1.add(new DefaultMutableTreeNode("M: 3.0"));
+							node_1.add(new DefaultMutableTreeNode("M: 4.0"));
+						add(node_1);
+						node_1 = new DefaultMutableTreeNode("Hard");
+							node_1.add(new DefaultMutableTreeNode("H: 1.0"));
+							node_1.add(new DefaultMutableTreeNode("H: 2.0"));
+							node_1.add(new DefaultMutableTreeNode("H: 3.0"));
+							node_1.add(new DefaultMutableTreeNode("H: 4.0"));
+						add(node_1);
+					}
+				}
+			));
+			ChallengesTab.add(ChallengeTree, "2,4");
+			
+			JLabel challengeLabel = new JLabel("Challenge Info");
+			challengePane.setViewportView(challengeLabel);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+/*****************************************************************************************************************************************
+* ChallengeTab initialization and components
+******************************************************************************************************************************************/			
+	
 	}
 }
