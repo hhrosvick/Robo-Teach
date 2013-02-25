fca44663cb720d3130574b24817bca32d4d8176b
Henry <hhrosvick@gmail.com>
2013-02-24 19:03:01 -0700

    Added more stubs and interface methods.

--- seng403/src/cpsc403/API.java
+++ seng403/src/cpsc403/API.java
@@ -9,8 +9,46 @@ public class API implements API_Interface {
 		// TODO Auto-generated constructor stub
 	}
 	
-	public Status startEmulator()	{
+	public Status initialize()	{
 		return CASAProcess.getInstance().abclEval("(load\"scripts/sim.lisp\")", null);
 	}
 
+	@Override
+	public Status initalaize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean authenticate_user(String user_name, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void loadToRobot(String filepath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadToSimulator(String filepath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String translateLoadToRobot(String filepath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String translateLoadToSimulator(String filepath) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
 }
