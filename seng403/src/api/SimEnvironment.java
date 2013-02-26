c1dd383d875ceaba15412266656d3c0545ac5645
Henry Rosvick <hhrosvick@gmail.com>
2013-02-26 01:21:05 -0700

    Added custom environment class

---
+++ seng403/src/api/SimEnvironment.java
@@ -0,0 +1,25 @@
+package api;
+
+import java.awt.Container;
+
+import casa.Agent;
+import casa.TransientAgent;
+import casa.abcl.ParamsMap;
+import casa.ui.AgentInternalFrame;
+import casa.ui.AgentUI;
+import casa.ui.TransientAgentInternalFrame;
+
+public class SimEnvironment extends iRobotCreate.simulator.Environment{
+
+	public SimEnvironment(ParamsMap params, AgentUI ui) throws Exception {
+		super(params, ui);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	protected TransientAgentInternalFrame makeDefaultInternalFrame(TransientAgent agent, String title, Container aFrame) {
+		return null;
+	 	//return new AgentInternalFrame((Agent)agent, title, aFrame);
+	}
+
+}
