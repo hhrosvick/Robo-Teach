7691d4a0f86b5f21bd067207f0ef19dc61371e55
David <david__ryan@hotmail.com>
2013-02-26 21:12:13 -0700

    Writin tests

--- seng403/tests/RoboTeachTestCases.java
+++ seng403/tests/RoboTeachTestCases.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+import gui.LessonsTab;
+import gui.ProgramTab;
+import gui.RoboTeach;
 
 import org.junit.Test;
 
@@ -6,8 +9,24 @@ import org.junit.Test;
 public class RoboTeachTestCases {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void RoboTeachInitializationTest() {
+		RoboTeach roboTestWindow = new RoboTeach();
+		assert(roboTestWindow != null);
+	}
+	@Test
+	public void LessonsTabInitializationTest() {
+		LessonsTab lessonsTestTab = new LessonsTab();
+		assert(lessonsTestTab != null);
+	}
+	@Test
+	public void ProgramTabInitializationTest() {
+		ProgramTab programTestTab = new ProgramTab();
+		assert(programTestTab != null);
+	}
+	@Test
+	public void ChallengesTabInitializationTest() {
+		ProgramTab challengesTestTab = new ProgramTab();
+		assert(challengesTestTab != null);
 	}
 
 }
