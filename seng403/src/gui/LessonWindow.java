a091952192ff49734e0eeed1cd91e379830e1ef0
Sonny <sonnyng.vzla@gmail.com>
2013-02-28 01:06:17 -0700

    changes in lesson window

--- seng403/src/gui/LessonWindow.java
+++ seng403/src/gui/LessonWindow.java
@@ -8,16 +8,39 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JToggleButton;
+import javax.swing.JSeparator;
+import javax.swing.JComboBox;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.awt.Button;
+import java.awt.Canvas;
+import javax.swing.JEditorPane;
+import javax.swing.JSpinner;
+import java.awt.Component;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class LessonWindow {
+public class LessonWindow{
 
 	private static JFrame frame;
 	private static String LessonName;
+	private final JButton btnPrevious = new JButton("Previous");
+	private final JButton btnNext = new JButton("Next");
+	private JFrame frame_1;
+	private int sectionNumber;
+	private String originalName;
+	
 // constructor takes lesson name argument
 	public LessonWindow(String LN) 
 	{
 		LessonName = LN;
+		originalName=LN;
+		frame_1 = new JFrame();
+		sectionNumber=0;
 		initialize();
+		
 	}
 // creates the new window
 	public static void OpenWindow() {
@@ -35,7 +58,40 @@ public class LessonWindow {
 	}
 // initializes components
 	private void initialize() {
-		setFrame(new JFrame());
+		
+		setFrame(frame_1);
+		frame_1.getContentPane().setLayout(new BoxLayout(frame_1.getContentPane(), BoxLayout.X_AXIS));
+		frame_1.getContentPane().add(btnNext);
+		frame_1.getContentPane().add(btnPrevious);
+		
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==btnNext){
+					sectionNumber+=1;
+					String nm= String.valueOf(sectionNumber);
+					LessonName=originalName+nm;
+					frame_1.getContentPane().removeAll();
+					OpenWindow();
+					initialize();
+				}
+			}
+		});
+		
+		btnPrevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==btnPrevious){
+					sectionNumber-=1;
+					String nm= String.valueOf(sectionNumber);
+					LessonName=originalName+nm;
+					frame_1.getContentPane().removeAll();
+					OpenWindow();
+					initialize();
+				}
+			}
+		});
+		
+		System.out.println(sectionNumber);
+		
 		getFrame().setBounds(100, 100, 450, 300);
 		// creates the image string and image icon
 		String imgStr = "Lessons/" + LessonName + ".png";
@@ -44,16 +100,19 @@ public class LessonWindow {
 		JScrollPane scrollPane = new JScrollPane();
 		getFrame().getContentPane().add(scrollPane, BorderLayout.CENTER);
 		// creates the label with the specified image icon
-
+		
 		JLabel LessonLabel = new JLabel("", LessonPicture, JLabel.CENTER);		
 		LessonLabel.setOpaque(true);
 		Color white = new Color(255,255,255);
 		LessonLabel.setBackground(white);
 		scrollPane.setViewportView(LessonLabel);
+		frame_1.pack();
 	}
+	
 	public static JFrame getFrame() {
 		return frame;
 	}
+	
 	public static void setFrame(JFrame frame) {
 		LessonWindow.frame = frame;
 	}
