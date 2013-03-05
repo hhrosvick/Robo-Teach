0649ce93b5a3264de5c2155f8938ba264db0def6
David <david__ryan@hotmail.com>
2013-03-04 17:23:04 -0700

    working on lesson window

--- seng403/src/gui/LessonWindowMod.java
+++ seng403/src/gui/LessonWindowMod.java
@@ -10,13 +10,19 @@ import com.jgoodies.forms.factories.FormFactory;
 import javax.swing.JPanel;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class LessonWindowMod {
 
 	private JFrame frame;
-
+	private static String LessonName;
+	private int sectionNumber;
+	private String originalName;
 	/**
 	 * Launch the application.
 	 */
@@ -24,7 +30,7 @@ public class LessonWindowMod {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LessonWindowMod window = new LessonWindowMod();
+					LessonWindowMod window = new LessonWindowMod(LessonName);
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -36,8 +42,13 @@ public class LessonWindowMod {
 	/**
 	 * Create the application.
 	 */
-	public LessonWindowMod() {
+	public LessonWindowMod(String LN) 
+	{
+		LessonName = LN;
+		originalName=LN;
+		sectionNumber=0;
 		initialize();
+		
 	}
 
 	/**
@@ -71,18 +82,29 @@ public class LessonWindowMod {
 		frame.getContentPane().add(SlidePanel, "2, 2, 1, 13, fill, fill");
 		SlidePanel.setLayout(new BorderLayout(0, 0));
 		
-		JLabel lblNewLabel = new JLabel("New label");
-		SlidePanel.add(lblNewLabel, BorderLayout.CENTER);
+		JLabel SlideLabel = new JLabel("New label");
+		SlidePanel.add(SlideLabel, BorderLayout.CENTER);
 		
 		JPanel ButtonPanel = new JPanel();
 		frame.getContentPane().add(ButtonPanel, "2, 16, fill, fill");
 		ButtonPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		ButtonPanel.add(btnNewButton_1);
+		JButton PreviousButton = new JButton("Previous");
+		PreviousButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String imgStr = "Lessons/" + LessonName + ".png";
+				final ImageIcon LessonPicture = new ImageIcon(imgStr);
+			}
+		});
+		ButtonPanel.add(PreviousButton);
 		
-		JButton btnNewButton = new JButton("New button");
-		ButtonPanel.add(btnNewButton);
+		JButton NextButton = new JButton("Next");
+		NextButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		ButtonPanel.add(NextButton);
 	}
 
 }
