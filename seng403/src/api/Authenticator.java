3ed2db399f41a8b44956f071e3a849b53eaabc89
jobzfirme <jsfirme@ucalgary.ca>
2013-03-15 02:30:28 -0600

    Changed == to .compareTo for better string comparison

--- seng403/src/api/Authenticator.java
+++ seng403/src/api/Authenticator.java
@@ -5,7 +5,7 @@ public class Authenticator {
 	public static int auth(String user, String pass){
 		
 		// TODO ITER 3: Implement authentication check to external system
-		if(user == "test" && pass == "1234") return 1;
+		if(user.equals("test") && pass.equals("1234")) return 1;
 		return 0;
 	}
 	
