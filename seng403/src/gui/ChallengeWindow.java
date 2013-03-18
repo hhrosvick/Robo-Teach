cdb14c5d12d8fb55d23c4b2bae905dd5fe1fc840
David <david__ryan@hotmail.com>
2013-03-18 15:13:04 -0600

    ChallengeWindow now uses api

--- seng403/src/gui/ChallengeWindow.java
+++ seng403/src/gui/ChallengeWindow.java
@@ -9,14 +9,28 @@ import javax.swing.JScrollPane;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 
+import api.API;
+import api.API_Interface;
+
 public class ChallengeWindow {
 
 	private static JFrame frame;
 	private static String ChallengeName;
+	private API_Interface api;
+	private int Tier;
+	private int Challenge;
 	// constructor takes lesson name argument
 	public ChallengeWindow(int t, int c, String CN) 
 	{
 		ChallengeName = CN;
+		Challenge = c;
+		Tier = t;
+		try {
+			api = new API();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		initialize();
 	}
 // creates the new window
@@ -38,8 +52,7 @@ public class ChallengeWindow {
 		setFrame(new JFrame());
 		getFrame().setBounds(100, 100, 450, 300);
 		// creates the image string and image icon
-		String imgStr = "Challenges/" + ChallengeName + ".png";
-		final ImageIcon ChallengePicture = new ImageIcon(imgStr);
+		final ImageIcon ChallengePicture = api.getChallenge(Tier, Challenge, 0);
 		// loads the scrollPane
 		JScrollPane scrollPane = new JScrollPane();
 		getFrame().getContentPane().add(scrollPane, BorderLayout.CENTER);
