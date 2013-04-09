360fbf9869a013af301c665013e08483d08fbbe4
Henry <hhrosvick@gmail.com>
2013-04-08 21:33:56 -0600

    Fixing api tests

--- seng403/tests/api/API_InterfaceTest.java
+++ seng403/tests/api/API_InterfaceTest.java
@@ -16,10 +16,10 @@ import api.API_Interface;
 
 public class API_InterfaceTest {
 
-	public API_Interface api = null;
+	public static API_Interface api = null;
 	
 	@BeforeClass
-	public void beforeTestClassSetup() {
+	public static void beforeTestClassSetup() {
 		try {
 			api = new API();
 		} catch (Exception e) {
@@ -103,12 +103,7 @@ public class API_InterfaceTest {
 	// get User Type tests
 	@Test
 	public void getUserTypeBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		int result = api.getUserType(-1);	
 		assertSame(result, 0);
 	}
@@ -116,12 +111,7 @@ public class API_InterfaceTest {
 	// get User Progress tests
 	@Test
 	public void getUserProgressBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		Map<String, String> result = api.getUserProgress(-1);	
 		assertSame(result, 0);
 	}
@@ -129,12 +119,7 @@ public class API_InterfaceTest {
 	// get All User Progress tests
 	@Test
 	public void getAllUserProgress(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		Map<Integer, Map<String, String>> result = api.getAllUserProgress();	
 		assertNotNull(result);
 	}
@@ -142,12 +127,7 @@ public class API_InterfaceTest {
 	// get All User IDs tests
 	@Test
 	public void getAllUserIDs(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		Vector<Integer> result = api.getAllUserIDs();	
 		assertNotNull(result);
 	}
@@ -155,159 +135,89 @@ public class API_InterfaceTest {
 	// get Lessons tests
 	@Test (expected=IllegalArgumentException.class)
 	public void getLessonAll3BadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.getLesson(-1, -2, -3);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void getLesson1stBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.getLesson(-1, 0, 0);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void getLesson2ndBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+	
 		api.getLesson(0, -1, 0);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void getLesson3rdBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.getLesson(0, 0, -1);	
 	}
 	
 	// get User Manual tests
 	@Test (expected=IllegalArgumentException.class)
 	public void getUserManualBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+	
 		api.getUserManual(-1);	
 	}
 	
 	// get Challenge tests
 	@Test (expected=IllegalArgumentException.class)
 	public void getChallengeFirst2BadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.getChallenge(-1, -2, false);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void getChallenge1stBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+	
 		api.getChallenge(-1, 0, true);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void getChallenge2ndBadInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.getChallenge(0, -1, false);	
 	}
 	
 	// set User Chapter tests
 	@Test (expected=Exception.class)
 	public void setUserChapterFirst2BadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChapter(-1, -2);	
 	}
 	
 	@Test (expected=Exception.class)
 	public void setUserChapter1stBadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChapter(-1, 0);	
 	}
 	
 	@Test (expected=Exception.class)
 	public void setUserChapter2ndBadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChapter(0, -1);	
 	}
 	
 	// set User Challenge tests
 	@Test (expected=Exception.class)
 	public void setUserChallengeFirst2BadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChallenge(-1, -2);	
 	}
 	
 	@Test (expected=Exception.class)
 	public void setUserChallenge1stBadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChallenge(-1, 0);	
 	}
 	
 	@Test (expected=Exception.class)
 	public void setUserChallenge2ndBadInput() throws Exception{
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.setUserChallenge(0, -1);	
 	}
 
@@ -317,46 +227,26 @@ public class API_InterfaceTest {
 	// load to robot tests
 	@Test (expected=IllegalArgumentException.class)
 	public void loadToRobotNullInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.loadToRobot(null);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void loadToRobotEmptyInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.loadToRobot("");	
 	}
 	
 	// load to simulator tests
 	@Test (expected=IllegalArgumentException.class)
 	public void loadToSimulatorNullInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.loadToRobot(null);	
 	}
 	
 	@Test (expected=IllegalArgumentException.class)
 	public void loadToSimulatorEmptyInput(){
-		API_Interface api = null;
-		try {
-			api = new API();
-		} catch (Exception e) {
-			fail();
-		}
+		
 		api.loadToRobot("");	
 	}
 	
